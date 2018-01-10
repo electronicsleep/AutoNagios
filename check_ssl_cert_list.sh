06c7ebb614cc11ab86efd4fec7fbb539e1eb3516
Chris <electronicsleep@gmail.com>
2018-01-10 00:02:01 -0800

    Linux SSL check

--- check_ssl_cert_list.sh
+++ check_ssl_cert_list.sh
@@ -17,18 +17,43 @@ do
  EXPIRY_DATE=$(echo $EXPIRY | cut -f 2 -d =)
  EXPIRY_YEAR=$(echo $EXPIRY_DATE | cut -f 4 -d ' ')
  EXPIRY_DAY=$(echo $EXPIRY_DATE | cut -f 2 -d ' ')
+ echo "EXPRY_DAY $EXPIRY_DAY"
  EXPIRY_MONTH=$(echo $EXPIRY_DATE | cut -f 1 -d ' ')
  echo "$EXPIRY"
 
  OS=$(uname)
  if [ "$OS" == "Darwin" ]; then
-  #MacOs
-  echo "Found MacOS"
+  #MacOS
   COMPARE=$(date -j -f "%d %b %Y %T %Z" "$EXPIRY_DAY $EXPIRY_MONTH $EXPIRY_YEAR 00:00:00 PST" +"%s")
  else
   #Linux
-  echo "Found Linux"
-  COMPARE=$(date "+%s" -d "$EXPIRY_YEAR-$EXPIRY_DAY-$EXPIRY_MONTH 00:00:00 PST")
+  if [ $EXPIRY_MONTH == "Jan" ]; then
+  EXPIRY_MONTH=01
+  elif [ $EXPIRY_MONTH == "Feb" ]; then
+  EXPIRY_MONTH=02
+  elif [ $EXPIRY_MONTH == "Mar" ]; then
+  EXPIRY_MONTH=03
+  elif [ $EXPIRY_MONTH == "Apr" ]; then
+  EXPIRY_MONTH=04
+  elif [ $EXPIRY_MONTH == "May" ]; then
+  EXPIRY_MONTH=05
+  elif [ $EXPIRY_MONTH == "Jun" ]; then
+  EXPIRY_MONTH=06
+  elif [ $EXPIRY_MONTH == "Jul" ]; then
+  EXPIRY_MONTH=07
+  elif [ $EXPIRY_MONTH == "Aug" ]; then
+  EXPIRY_MONTH=08
+  elif [ $EXPIRY_MONTH == "Sep" ]; then
+  EXPIRY_MONTH=09
+  elif [ $EXPIRY_MONTH == "Oct" ]; then
+  EXPIRY_MONTH=10
+  elif [ $EXPIRY_MONTH == "Nov" ]; then
+  EXPIRY_MONTH=11
+  elif [ $EXPIRY_MONTH == "Dec" ]; then
+  EXPIRY_MONTH=12
+  fi
+
+  COMPARE=$(date -d "$EXPIRY_MONTH/$EXPIRY_DAY/$EXPIRY_YEAR 00:00:00" +"%s")
  fi
  
  SEC=$(expr $COMPARE - $EPOC)
